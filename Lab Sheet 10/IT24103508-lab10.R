ca72cb38c91f26719590494571f38c06f216aca6
IT24103508 <IT24103508@my.sliit.lk>
2025-10-23 14:28:53 +0530

    Add files via upload

---
+++ Lab Sheet 10/IT24103508-lab10.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\Venura Jayawardana\\Music")
+
+#Excercise
+
+#i
+observed_counts <- c(120, 95, 85, 100)
+
+
+#ii
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+
+#iii
+chisq.test(x = observed_counts, p = probabilities)
